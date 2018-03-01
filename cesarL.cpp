a42cee6d5c4a409837cfa3e6b07d9187891abb8c
Cesar Lara <clara@cs.csubak.edu>
2018-02-28 18:28:46 -0800

    tutorial menu functionality added

--- cesarL.cpp
+++ cesarL.cpp
@@ -53,13 +53,25 @@ public:
 
 };
 
+class Global_tutorial : public Global {
+public:
+	int screen;
+	Global_tutorial() {
+		xres = 1250;
+		yres = 900;
+		memset(keys, 0, 65536);
+		screen = 1;
+	}
+};
+
 // Function prototypes
 //
 void menu_render(Global_menu &gl);
 void menu_opengl(Global_menu &gl);
 int menu_check_keys(XEvent *e, Global_menu &gl);
+//-----------------------------------------------------------------------------
 // Main Menu Function
-// 
+//----------------------------------------------------------------------------- 
 
 int menu() {
         logOpen();
@@ -133,12 +145,21 @@ int menu_check_keys(XEvent *e, Global_menu &gl) {
 			if (gl.option > 0)
 				gl.menuUp();
 			break;
+		case XK_Up:
+			if (gl.option > 0)
+				gl.menuUp();
+			break;
                 case XK_s:
 			if (gl.option < 3)
 				gl.menuDown();
 			break;
+		case XK_Down:
+			if (gl.option < 3)
+				gl.menuDown();
+			break;
 		case XK_e:
-			cout << "enter was pressed\n";
+			return (gl.option+1);
+		case XK_Return:
 			return (gl.option+1);
         }
         return 0;
@@ -196,14 +217,14 @@ void menu_render(Global_menu &gl) {
 	s = &gl.box[gl.option];
         glColor3ub(255, 255, 255);
         glPushMatrix();
-        glTranslatef(865, s->center.y , s->center.z);
+        glTranslatef(880, s->center.y , s->center.z);
         glBegin(GL_TRIANGLES);
-                glVertex2f(-40.0f, 0.0f);
-                glVertex2f(  0.0f, 40.0f);
+                glVertex2f(-55.0f, 0.0f);
+                glVertex2f(  0.0f, 30.0f);
                 glVertex2f(  0.0f, 0.0f);
-                glVertex2f(  0.0f, -40.0f);
+                glVertex2f(  0.0f, -30.0f);
                 glVertex2f(  0.0f, 0.0f);
-                glVertex2f( -40.0f, 0.0f);
+                glVertex2f( -55.0f, 0.0f);
         glEnd();
 	glBegin(GL_POINTS);
 		glVertex2f(0.0f, 0.0f);
@@ -211,15 +232,124 @@ void menu_render(Global_menu &gl) {
 	glPopMatrix();
 
 }
-
+//----------------------------------------------------------------------------
 // Tutorial Menu-state
+//----------------------------------------------------------------------------
+// Function prototypes
 //
+void tutorial_opengl(Global_tutorial &gl);
+int tutorial_keys(XEvent *e, Global_tutorial &gl);
+void tutorial_render(Global_tutorial &gl);
+
 int tutorial() {
+        Global_tutorial gl;
+        X11_wrapper x11(gl);
+        tutorial_opengl(gl);
+        srand(time(NULL));
+        x11.set_mouse_position(100, 100);
+        int done=0;
+        while (done==0) {
+                while (x11.getXPending()) {
+                        XEvent e = x11.getXNextEvent();
+                        x11.check_resize(&e, gl);
+                        done = tutorial_keys(&e, gl);
+                }
+                tutorial_render(gl);
+                x11.swapBuffers();
+        }
+
+        cleanup_fonts();
 	return 0;
 }
 
-// High Scores Menu-state
+//Function definitions
 //
+void tutorial_opengl(Global_tutorial &gl) {
+        //OpenGL initialization
+        glViewport(0, 0, gl.xres, gl.yres);
+        //Initialize matrices
+        glMatrixMode(GL_PROJECTION); glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+        //This sets 2D mode (no perspective)
+        glOrtho(0, gl.xres, 0, gl.yres, -1, 1);
+        //
+        glDisable(GL_LIGHTING);
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_FOG);
+        glDisable(GL_CULL_FACE);
+        //
+        //Clear the screen to black
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        //Do this to allow fonts
+        glEnable(GL_TEXTURE_2D);
+        initialize_fonts();
+
+}
+
+int tutorial_keys(XEvent *e, Global_tutorial &gl) {
+        //keyboard input?
+        static int shift=0;
+        if (e->type != KeyPress && e->type != KeyRelease)
+                return 0;
+        int key = (XLookupKeysym(&e->xkey, 0) & 0x0000ffff);
+        //Log("key: %i\n", key);
+        if (e->type == KeyRelease) {
+                gl.keys[key]=0;
+                if (key == XK_Shift_L || key == XK_Shift_R)
+                        shift=0;
+                return 0;
+        }
+        gl.keys[key]=1;
+        if (key == XK_Shift_L || key == XK_Shift_R) {
+                shift=1;
+                return 0;
+        }
+        (void)shift;
+        switch (key) {
+                case XK_Escape:
+                        if (gl.screen > 1)
+				gl.screen -= 1;
+			else
+				return 1;
+			break;
+                case XK_e:
+			if (gl.screen < 4)
+                               gl.screen += 1;
+                        else
+                                return 1;
+			break;
+                case XK_Return:
+                        if (gl.screen < 4)
+				gl.screen += 1;
+			else
+				return 1;
+			break;
+        }
+	return 0;
+}
+
+void tutorial_render(Global_tutorial &gl) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	Rect r;
+	r.bot = 700;
+	r.center = 0;
+	r.left = 625;
+	if (gl.screen == 1) {
+		ggprint16(&r, 16, 0x00ffffff, "Intro");
+	}
+	if (gl.screen == 2) {
+		ggprint16(&r, 16, 0x00ffffff, "Controls");
+	}
+        if (gl.screen == 3) {
+                ggprint16(&r, 16, 0x00ffffff, "Extra Info");
+        }
+        if (gl.screen == 4) {
+                ggprint16(&r, 16, 0x00ffffff, "Extra Info 2");
+        }
+}
+//----------------------------------------------------------------------------
+// High Scores Menu-state
+//----------------------------------------------------------------------------
 int highScores() {
 	return 0;
 }
